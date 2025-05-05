e55080a20815f1da69a989cd523dbfff35433d09
zhaohai@tydic.com <zhaohai@tydic.com>
2025-05-05 22:33:23 +0800

    add start.sh and stop.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,25 @@
-nohup /usr/local/jdk1.8.0_131/bin/java -Dfile.encoding=UTF-8  -jar  easyCallcenter365.jar > /dev/null 2>&1 &
+echo "The service is being launched..."
+
+echo "This will probably take 35 to 60 seconds. "
+
+echo "Try to start mysql-server..."
+docker start  mysql
+
+echo "Try to start funasr..."
+docker start  funasr-online-2710
+sleep 15
+
+echo "Try to start freeswitch-docker..."
+docker start  freeswitch-debian12
+sleep 5
+
+echo "Try to start easycallcenter365.jar "
+nohup /usr/local/jdk1.8.0_131/bin/java  -Dfile.encoding=UTF-8  -jar  easycallcenter365.jar > /dev/null 2>&1 &
+sleep 5
+
+echo "Try to start callcenter-manager.jar"
+nohup /usr/local/jdk1.8.0_131/bin/java  -Dfile.encoding=UTF-8  -jar  callcenter-manager.jar > /dev/null 2>&1 &
+
+echo " "
+echo "Done."
+echo " "
---
+++ stop.sh
@@ -0,0 +1,22 @@
+echo "The service is being shutdown..."
+
+echo "This will probably take 30 seconds. "
+
+echo "Try to stop mysql-server..."
+docker stop  mysql
+
+echo "Try to stop funasr..."
+docker stop  funasr-online-2710
+sleep 3
+
+echo "Try to stop freeswitch-docker..."
+docker stop  freeswitch-debian12
+sleep 3
+
+echo "Try to stop easycallcenter365.jar "
+echo "Try to stop callcenter-manager.jar"
+pkill -9 java
+
+echo " "
+echo "Done."
+echo " "
